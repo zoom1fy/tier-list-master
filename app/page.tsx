2784d33222ea098106026a61750b97aa4f7da35e
Ivan <yutub.lol@yandex.ru>
2026-07-05 13:24:47 +0300

    add autobuild

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
+const basePath = "/tier-lisr-master";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+        <img
           className="dark:invert"
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
-          priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
@@ -41,9 +40,9 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
+            <img
               className="dark:invert"
-              src="/vercel.svg"
+              src={`${basePath}/vercel.svg`}
               alt="Vercel logomark"
               width={16}
               height={16}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/tier-lisr-master",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
