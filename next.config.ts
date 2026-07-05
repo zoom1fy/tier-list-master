9a168e57dd3561170d85d8fcd3fb84c1e7968b41
Ivan <yutub.lol@yandex.ru>
2026-07-05 13:38:25 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/tier-lisr-master" : "",
+  basePath: process.env.PAGES_BASE_PATH || process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
   },
